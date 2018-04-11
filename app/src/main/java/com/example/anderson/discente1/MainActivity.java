d0554e8ac4bbb71a1aa8ead520f8c9cba29ab584
Anderson Antonio Fontana <andersonfontana14@hotmail.com>
2018-04-11 11:36:50 -0300

    Criação do projeto

---
+++ app/src/main/java/com/example/anderson/discente1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.anderson.discente1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+//    https://webmaniabr.com/api/1/cep/99052-270/?app_key=vraaek5FzAKx8MLytjfFaInAbH9dGHGy&app_secret=Qco7VOwYyTShUKEM5yAdnrn28sXqh5EU8sHRldeq9iq0FOyb
+}
