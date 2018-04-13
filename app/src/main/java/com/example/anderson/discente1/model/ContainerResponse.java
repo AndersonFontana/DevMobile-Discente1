77eecbc5454f5c50f1c9bd42a5f360d83c120a95
Anderson Antonio Fontana <andersonfontana14@hotmail.com>
2018-04-13 00:18:25 -0300

    Http e intent

---
+++ app/src/main/java/com/example/anderson/discente1/model/ContainerResponse.java
@@ -0,0 +1,8 @@
+package com.example.anderson.discente1.model;
+
+/**
+ * Created by Anderson on 12/04/18.
+ */
+
+public class ContainerResponse {
+}
---
+++ app/src/main/java/com/example/anderson/discente1/retrofit/DefinicaoServico.java
@@ -0,0 +1,8 @@
+package com.example.anderson.discente1.retrofit;
+
+/**
+ * Created by Anderson on 12/04/18.
+ */
+
+public interface DefinicaoServico {
+}
